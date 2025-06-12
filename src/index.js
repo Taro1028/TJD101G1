797562f5a8e1ec44e98579001601b6c52ebc1bdf
Kevin224055 <g7z916b@gmail.com>
2025-06-12 10:13:37 +0800

    adjust：Stash pop conflict

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import router from "@/router/index.js";
-import 'normalize.css';
+import "normalize.css";
 // import 'bootstrap';
 // import 'bootstrap-icons/font/bootstrap-icons.css'
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import './assets/sass/main.scss';
+import "./assets/sass/main.scss";
 import Main from "@/Main.vue";
 import 'leaflet/dist/leaflet.css';
 import { useMemberStore } from "@/stores/MemberStore";
